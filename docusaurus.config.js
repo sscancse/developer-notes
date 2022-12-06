402e99a8a27aa116ea8fbd631fb6f283912a6d4c
Seamus_Scanlon <Seamus_C_Scanlon@Progressive.com>
2022-12-06 17:58:13 -0500

    First two days of notes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Development Notes',
+  title: 'Seamus Scanlon Developer Notes',
   tagline: 'Notes about Software Development',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
